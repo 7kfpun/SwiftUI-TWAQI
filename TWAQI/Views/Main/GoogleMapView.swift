12b3d568230ff1bc9b88ffc57610429162ebccef
kf <7kfpun@gmail.com>
2019-10-26 15:20:50 +0800

    refactor: google map marker

--- TWAQI/Views/Main/GoogleMapView.swift
+++ TWAQI/Views/Main/GoogleMapView.swift
@@ -12,11 +12,40 @@ import UIKit
 
 final class PollutantMarker: GMSMarker {
     let pollutant: Pollutant
-    let isWindMode: Bool
 
-    init(pollutant: Pollutant, isWindMode: Bool) {
+    init(pollutant: Pollutant) {
         self.pollutant = pollutant
-        self.isWindMode = isWindMode
+        super.init()
+
+        guard let dLatitude: Double = Double(pollutant.latitude),
+            let dLongitude: Double = Double(pollutant.longitude) else {
+           return
+        }
+        self.position = CLLocationCoordinate2D(latitude: dLatitude, longitude: dLongitude)
+
+        let airStatus = Constants.AirIndexTypes.aqi.getAirStatus(value: Int(pollutant.aqi) ?? 0)
+        let color = UIColor(rgb: Int(airStatus.getColor()))
+        let foregroundColor = UIColor(rgb: Int(airStatus.getForegroundColor()))
+
+        let label = UILabel(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 40, height: 30)))
+        label.text = pollutant.aqi
+        label.textColor = foregroundColor
+        label.font = UIFont.systemFont(ofSize: 15.0, weight: .thin)
+        label.backgroundColor = color
+        label.textAlignment = NSTextAlignment.center
+        label.layer.borderColor = UIColor.white.cgColor
+        label.layer.borderWidth = 1.0
+        label.layer.cornerRadius = 8
+        label.layer.masksToBounds = true
+        self.iconView = label
+    }
+}
+
+final class WindDirectionMarker: GMSMarker {
+    let pollutant: Pollutant
+
+    init(pollutant: Pollutant) {
+        self.pollutant = pollutant
         super.init()
 
         guard let dLatitude: Double = Double(pollutant.latitude),
@@ -29,29 +58,14 @@ final class PollutantMarker: GMSMarker {
 
         let airStatus = Constants.AirIndexTypes.aqi.getAirStatus(value: Int(pollutant.aqi) ?? 0)
         let color = UIColor(rgb: Int(airStatus.getColor()))
-        let foregroundColor = UIColor(rgb: Int(airStatus.getForegroundColor()))
 
-        if isWindMode {
-            let boldConfig = UIImage.SymbolConfiguration(weight: .bold)
-            let icon = UIImage(systemName: "arrow.down", withConfiguration: boldConfig)!
-                .withTintColor(color, renderingMode: .alwaysOriginal)
-                .resizeImage(targetSize: CGSize(width: dWindSpeed * 5, height: dWindSpeed * 5))
-                .addShadow()
-            self.icon = icon
-            self.rotation = dWindDirection
-        } else {
-            let label = UILabel(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 40, height: 30)))
-            label.text = pollutant.aqi
-            label.textColor = foregroundColor
-            label.font = UIFont.systemFont(ofSize: 15.0, weight: .thin)
-            label.backgroundColor = color
-            label.textAlignment = NSTextAlignment.center
-            label.layer.borderColor = UIColor.white.cgColor
-            label.layer.borderWidth = 1.0
-            label.layer.cornerRadius = 8
-            label.layer.masksToBounds = true
-            self.iconView = label
-        }
+        let boldConfig = UIImage.SymbolConfiguration(weight: .bold)
+        let icon = UIImage(systemName: "arrow.down", withConfiguration: boldConfig)!
+            .withTintColor(color, renderingMode: .alwaysOriginal)
+            .resizeImage(targetSize: CGSize(width: dWindSpeed * 5, height: dWindSpeed * 5))
+            .addShadow()
+        self.icon = icon
+        self.rotation = dWindDirection
     }
 }
 
@@ -72,8 +86,14 @@ struct GoogleMapView: UIViewRepresentable {
     /// Updates the presented `UIView` (and coordinator) to the latest configuration.
     func updateUIView(_ mapView: GMSMapView, context: Self.Context) {
         mapView.clear()
+
         pollutants.forEach { pollutant in
-            let pollutantMarker = PollutantMarker(pollutant: pollutant, isWindMode: isWindMode)
+            var pollutantMarker: GMSMarker
+            if isWindMode {
+                pollutantMarker = WindDirectionMarker(pollutant: pollutant)
+            } else {
+                pollutantMarker = PollutantMarker(pollutant: pollutant)
+            }
             pollutantMarker.map = mapView
         }
     }
